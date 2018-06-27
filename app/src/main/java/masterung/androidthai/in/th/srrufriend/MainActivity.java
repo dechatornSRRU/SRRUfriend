b48d348dd03327075b6f49a25c41b602b0e9afd9
Win10 <dechatorn@srru.ac.th>
2018-06-27 16:23:23 +0700

    finish 27 june

--- app/src/main/java/masterung/androidthai/in/th/srrufriend/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/srrufriend/MainActivity.java
@@ -1,7 +1,10 @@
 package masterung.androidthai.in.th.srrufriend;
 
+import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import masterung.androidthai.in.th.srrufriend.library.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+            Add Fragment to acctivity
+        if (savedInstanceState ==null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+            }
+
+
+
+
+    } // Main Method
+
+} // Main Class
